444280753bb0bbbf24e8767a8ee5164b8b6748e7
Brian Clifton <brian@clifton.me>
2016-03-22 18:48:19 -0700

    - Removed reference to concat (wasn't needed) - Stubbed out learn page

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,51 +11,35 @@ module.exports = function(grunt) {
         ]
       }
     },
-    concat: {
-      js: {
-        src: [
-          'bower_components/jquery/dist/jquery.min.js',
-          'bower_components/handlebars/handlebars.min.js',
-          'bower_components/bootstrap/dist/js/bootstrap.min.js',
-          'js/app.js'
-        ],
-        dest: 'dist/js/<%= pkg.name %>.js'
-      },
-      css: {
-        src: [
-          'bower_components/bootstrap/dist/css/bootstrap.min.css',
-          'css/app.css'
-        ],
-        dest: 'dist/css/<%= pkg.name %>.css'
-      }
-    },
     uglify: {
       target: {
         files: {
-          'dist/js/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
+          'dist/js/<%= pkg.name %>.min.js': [
+            'bower_components/jquery/dist/jquery.min.js',
+            'bower_components/handlebars/handlebars.min.js',
+            'bower_components/bootstrap/dist/js/bootstrap.min.js',
+            'js/app.js'
+          ]
         }
       }
     },
     cssmin: {
       target: {
         files: {
-          'dist/css/<%= pkg.name %>.min.css': ['<%= concat.css.dest %>']
+          'dist/css/<%= pkg.name %>.min.css': [
+            'bower_components/bootstrap/dist/css/bootstrap.min.css',
+            'css/app.css'
+          ]
         }
       }
     },
-    clean: {
-      dist: [ "dist/" ],
-      'combined-files': [
-        "<%= concat.js.dest %>",
-        "<%= concat.css.dest %>"
-      ],
-    },
+    clean: [ "dist/" ],
     'compile-handlebars': {
       target:{
-        files: [{
-          src: 'hbs/index.hbs',
-          dest: 'dist/index.html'
-        }],
+        files: [
+          { src: 'hbs/index.hbs', dest: 'dist/index.html' },
+          { src: 'hbs/learn.hbs', dest: 'dist/learn/index.html' }
+        ],
         templateData: 'hbs/config.json'
       }
     },
@@ -81,13 +65,12 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-compile-handlebars');
   grunt.loadNpmTasks('grunt-ssh-deploy');
 
-  grunt.registerTask('default', ['clean:dist', 'copy', 'concat', 'uglify', 'cssmin', 'clean:combined-files', 'compile-handlebars']);
+  grunt.registerTask('default', ['clean', 'copy', 'uglify', 'cssmin', 'compile-handlebars']);
   grunt.registerTask('deploy', ['default', 'ssh_deploy:production']);
 };
