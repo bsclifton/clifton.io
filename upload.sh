e5dfb6c98ce1ee44725dc4c6868446e81aee1454
Brian Clifton <brian@clifton.me>
2024-01-27 12:27:04 -0700

    Fixed upload script

--- upload.sh
+++ upload.sh
@@ -1,9 +1,10 @@
 echo "BUILDING"
-be jekyll build
+bundle exec jekyll build
 pushd .
 cd _site
 echo "PACKAGING"
 tar -czf ../site.tar.gz .
+popd
 scp ./site.tar.gz brian@clifton.io:~/
 rm site.tar.gz
 echo "UPLOAD COMPLETE"
